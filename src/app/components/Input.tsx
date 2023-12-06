e64c00ba43a2985c3e0a3a668e1795a0f418fa55
lucas <lucasrodrigues.sousa09@gmail.com>
2023-12-06 11:47:43 -0300

    Remove input autofill bg-color

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -11,6 +11,6 @@ type InputProps = {
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(({id, name, onBlur, onChange, type = 'text'}, ref) => {
     return(
-        <input id={id} name={name} onBlur={onBlur} onChange={onChange} type={type} ref={ref} className="block p-2 text-primary w-full bg-background border border-primary rounded h-12" />
+        <input id={id} name={name} onBlur={onBlur} onChange={onChange} type={type} ref={ref} className="block p-2 text-primary w-full bg-background border border-primary rounded h-12 focus:outline-primary" />
     )
 })
