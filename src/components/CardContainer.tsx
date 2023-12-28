6f6b60d8428d8227517ef69d16b0267ecd23ab84
lucas <lucasrodrigues.sousa09@gmail.com>
2023-12-28 18:39:04 -0300

    Get products from stripe

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,14 +1,31 @@
-import { prisma } from "../lib/prisma";
+import { stripe } from "../lib/stripe";
+import { formatter } from "../lib/formatter";
+
 import { Card } from "./Card";
+import Stripe from "stripe";
 
-async function getPizzasData(){
-    const pizzasData = await prisma.pizza.findMany()
+async function getPizzasFromStripe(){
+    const product = await stripe.products.list({
+        expand: ['data.default_price']
+    })
 
-    return pizzasData
+    const pizzas = product.data.map(pizza => {
+        const price = pizza.default_price as Stripe.Price
+
+        return {
+            id: pizza.id,
+            name: pizza.name,
+            price: price.unit_amount || 0,
+            image: pizza.images[0]
+        }
+    })
+
+    return pizzas
 }
 
+
 export async function CardContainer(){
-    const pizzas = await getPizzasData()
+    const pizzas = await getPizzasFromStripe()
 
     return (
         <div className="grid grid-cols-4 max-w-[1456px] items-center justify-center mb-16 mx-auto gap-20">
@@ -16,7 +33,7 @@ export async function CardContainer(){
             pizzas.length > 0 ?
             pizzas.map(pizza => {
                 return (
-                    <Card key={pizza.id} pizzaName={pizza.name} price={pizza.price} pizzaImg={pizza.img}  />
+                    <Card key={pizza.id} pizzaName={pizza.name} price={pizza.price} pizzaImg={pizza.image}  />
                 )
             }) :
             <strong>Não há pizzas cadastradas</strong>
