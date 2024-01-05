b397a776dbe639e3ae9d89b5552ac47406e5dfb6
lucas <lucasrodrigues.sousa09@gmail.com>
2024-01-05 10:44:02 -0300

    Create checkout stripe route

---
+++ src/app/api/checkout/route.ts
@@ -0,0 +1,14 @@
+import { stripe } from "@/lib/stripe";
+
+export async function POST(req: Request) {
+    const body = await req.json()
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: body.map((lineItem: {price: string, quantity: number}) => lineItem ),
+        mode: 'payment',
+        success_url: `http://localhost:3000/success`,
+        cancel_url: `http://localhost:3000/`,
+      });
+
+    return Response.json({checkoutSession: session.url})
+}
