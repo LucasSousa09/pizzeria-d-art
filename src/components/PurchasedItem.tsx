32eacc1d503acab55b026f01f84f233561a6ed63
lucas <lucasrodrigues.sousa09@gmail.com>
2024-01-10 18:44:27 -0300

    Add date-fns to format order date

--- src/components/PurchasedItem.tsx
+++ src/components/PurchasedItem.tsx
@@ -1,18 +1,29 @@
-import { CaretDoubleDown } from '@phosphor-icons/react/dist/ssr'
 import Image from 'next/image'
+import { format } from 'date-fns'
+import { CaretDoubleDown } from '@phosphor-icons/react/dist/ssr'
 
-export function PurchasedItem(){
+import { formatter } from '@/lib/formatter'
+
+type PurchasedItemProps = {
+    pizzaImg: string,
+    createdAt: Date,
+    totalPrice: number,
+} 
+
+export function PurchasedItem({ pizzaImg, createdAt, totalPrice }: PurchasedItemProps){
+    const date = format(createdAt, 'dd/MM/yyyy - hh:mm')
+
     return (
         <div className="flex items-start p-5 gap-5 bg-primary text-white rounded">
-            <Image src={''} alt="" height={90} width={90} />
+            <Image src={pizzaImg} alt="" height={90} width={90} />
 
             <div className="flex flex-col justify-between items-end text-lg h-full">
                 <div className='flex flex-col items-end gap-2'>
-                    <strong className="leading-tight">08/11/2023 - 21:32</strong>
-                    <strong className="leading-tight">R$ 131,96</strong>
+                    <strong className="leading-tight">{ date }</strong>
+                    <strong className="leading-tight">{ formatter.format(totalPrice / 100)}</strong>
                 </div>
 
-                <button className='flex items-center gap-1 font-medium text-xs'>Ver detalhes <CaretDoubleDown weight="bold" size={15} /> </button>
+                <button className='flex items-center gap-1 font-medium text-xs'>Ver detalhes<CaretDoubleDown weight="bold" size={15} /> </button>
             </div>
         </div>
     )
