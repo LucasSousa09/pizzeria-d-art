5799b2c62dc84cda4d6f12c0b8aa1e70a465be60
lucas <lucasrodrigues.sousa09@gmail.com>
2024-01-11 18:43:34 -0300

    Pass session_id through success_url

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -3,12 +3,19 @@ import { stripe } from "@/lib/stripe";
 export async function POST(req: Request) {
     const body = await req.json()
 
-    const session = await stripe.checkout.sessions.create({
-        line_items: body.map((lineItem: {price: string, quantity: number}) => lineItem ),
-        mode: 'payment',
-        success_url: `http://localhost:3000/success`,
-        cancel_url: `http://localhost:3000/checkout`,
-      });
-
-    return Response.json({checkoutSession: session.url})
+    try {
+      const session = await stripe.checkout.sessions.create({
+          line_items: body.map((lineItem: {price: string, quantity: number}) => lineItem ),
+          mode: 'payment',
+          success_url: `http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `http://localhost:3000/checkout`,
+        });
+      
+        return Response.json({checkoutSession: session.url})
+    }
+    catch(err){
+      return new Response('Failure at creating session', {
+        status: 402
+      })
+    }
 }
