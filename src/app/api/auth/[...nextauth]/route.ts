6871c6445ea74f786becc429efb609438c71222c
lucas <lucasrodrigues.sousa09@gmail.com>
2024-01-16 12:33:09 -0300

    Add google provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,22 @@
-import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
-import { prisma } from "../../../../lib/prisma";
+import { NextAuthOptions } from "next-auth";
 
-export const handler = NextAuth({
+import NextAuth from "next-auth/next";
+
+import { prisma } from "@/lib/prisma";
+
+const authOptions: NextAuthOptions = {
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID ?? "",
             clientSecret: process.env.GITHUB_SECRET ?? "",   
-        })
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+          })     
     ],
     callbacks: {
         async signIn({ user }) {
@@ -38,7 +46,10 @@ export const handler = NextAuth({
                 return false
             }
         },
-    }
-})
+    },
+
+}
+
+const handler = NextAuth(authOptions)
 
 export {handler as GET, handler as POST}
