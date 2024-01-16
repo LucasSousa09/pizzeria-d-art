43bfce55aec33ec57777f987b56aae6c448c6215
lucas <lucasrodrigues.sousa09@gmail.com>
2024-01-16 12:33:31 -0300

    Add google provider

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'files.stripe.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com'
             }
     ]
     }    
