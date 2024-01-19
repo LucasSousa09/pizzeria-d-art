d2afee60ee120eeeea358d7d5776f391401a5fa2
lucas <lucasrodrigues.sousa09@gmail.com>
2024-01-19 10:04:51 -0300

    Add cache to stripe requisition

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,14 +1,25 @@
+import { cache } from "react";
 import { stripe } from "../lib/stripe";
 
 import { Card } from "./Card";
 import Stripe from "stripe";
 
-async function getPizzasFromStripe(){
+export const revalidate = 3600 
+
+const getPizzasFromStripe = cache(async () => {
     const product = await stripe.products.list({
         expand: ['data.default_price']
     })
 
-    const pizzas = product.data.map(pizza => {
+    const { data } = product
+
+    return data
+})
+
+export async function CardContainer(){
+    const pizzasData = await getPizzasFromStripe()
+    
+    const pizzas = pizzasData.map(pizza => {
         const price = pizza.default_price as Stripe.Price
 
         return {
@@ -18,13 +29,6 @@ async function getPizzasFromStripe(){
             image: pizza.images[0]
         }
     })
-
-    return pizzas
-}
-
-
-export async function CardContainer(){
-    const pizzas = await getPizzasFromStripe()
 
     return (
         <div className="grid xs:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 content-center justify-items-center gap-y-10 xs:gap-2 sm:gap-10 md:gap-4 lg:gap-12 2xl:gap-20 max-w-[1456px] xs:px-2 mb-16 2xl:mx-auto">
