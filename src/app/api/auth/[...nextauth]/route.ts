1f09439ad68b88f68bf6c07c1b9f8ccc501b75eb
lucas <lucasrodrigues.sousa09@gmail.com>
2024-03-06 16:31:15 -0300

    Add credentials provider to next-auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,11 @@
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
-
-import { NextAuthOptions } from "next-auth";
+import CredentialsProvider  from "next-auth/providers/credentials"
 
 import NextAuth from "next-auth/next";
+import { NextAuthOptions } from "next-auth";
+
+import bcrypt from 'bcrypt'
 
 import { prisma } from "@/lib/prisma";
 
@@ -16,17 +18,63 @@ const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-          })     
+          }),
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                username: {label: "Username", type: "text", placeholder: "lucas"},
+                password: {label: "Password", type: "password"},
+                email: {label: "Email", type:"text"}
+            },
+            async authorize(credentials) {
+                // Check if email and password are valid
+                if(credentials !== undefined && (!credentials.email || !credentials.password)) {
+                    return null
+                }
+
+                //Check if the user exists
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials?.email
+                    }
+                })
+
+                if(!user){
+                    return null
+                }
+
+                //Check if the passwords match
+                const passwordsMatch = await bcrypt.compare(credentials?.password || "", user.password || "")
+
+                if(!passwordsMatch){
+                    return null
+                }
+
+                return user
+            }
+        })    
     ],
     callbacks: {
         async signIn({ user }) {
             const { name, email, image } = user
+
             try {
                 const emailFound = await prisma.user.findUnique({
                     where: {
                         email: email || ''
                     }
                 })
+
+                if(user.avatar === undefined && emailFound && image && email && name){
+                    await prisma.user.update({
+                        where: {
+                            email: email
+                        },
+                        data: {
+                            avatar: image
+                        }
+                    })                                       
+                }
 
                 if(emailFound === null){
                     if(email && name){
@@ -47,7 +95,37 @@ const authOptions: NextAuthOptions = {
                 return false
             }
         },
+        jwt: async ({token, user}) => {
+            if(user && !user.name){
+                token.name = user.username
+                token.email = user.email
+
+                if(user.avatar === null){
+                    token.picture = user.avatar
+                }
+            }
+
+            if(user && user.name){
+                token.name = user.name
+                token.email = user.email
+            }
+
+            return token
+        },
+        session: async ({session, token}) => {
+            session.user = {
+                name: token.name,
+                image: token.picture
+            }
+            
+            return session
+        }
     },
+    session: {
+        strategy: "jwt"
+    },
+
+    secret: process.env.NEXTAUTH_SECRET
 
 }
 
