a171cbc6035e75fb0f83be6e91ed2fd461b42e9a
lucas <lucasrodrigues.sousa09@gmail.com>
2024-03-06 16:52:25 -0300

    Update profile page; update styles and errors handling

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
-import { PencilSimple } from "@phosphor-icons/react/dist/ssr";
+import { Plus } from "@phosphor-icons/react/dist/ssr";
 
-import { Order } from "../../components/Order";
 import { ProfileBox } from "../../components/ProfileBox";
+import { FormProfile } from "../../components/Form/FormProfile";
 
+import { api } from "@/lib/axios";
 import { prisma } from "@/lib/prisma";
+
+import type { AddressData } from '../../components/Form/FormAddress' 
+import { OrdersBox } from "@/components/OrdersBox";
+import { AddressesBox } from "@/components/AddressesBox";
 
 export type Pizza = {
     id: string,
@@ -16,22 +21,34 @@ export type Pizza = {
     quantity: number
 }
 
-const payment = {
-    money: 'Dinheiro',
-    pix: 'PIX',
-    credit: 'Cartão de Crédito',
+type ProfileData = {
+        id: string,
+        email: string,
+        username: string,
+        avatar: string | null,
+        cpf: string | null,
+        phone: string | null,
+        mainAddress: string | null
 }
-
-const pizzaStatus = {
-    pending: 'Em andamento',
-    finished: 'Concluída',
-    canceled: 'Cancelada'
-}
-
-
 
 export default async function Profile(){
     const session = await getServerSession()
+
+    if(session === null || session.user === undefined){
+        redirect('/login?error=session+error')
+    }
+    
+    const { data: profileData, status } :  { data: ProfileData, status: number } = await api.post('/get-profile-data',{email: session.user.email})
+    const { data: addressesData, status: addressesStatus } : {data: AddressData[], status: number } = await api.post('/get-addresses-data', {email: session.user.email})
+
+    if(status === 403 || addressesStatus === 403){
+        redirect('/profile?error=user+not+found')
+    }
+
+    if(status === 500 || addressesStatus === 500){
+        redirect('/profile?error=random+error')
+    }
+
 
     const userId = await prisma.user.findUnique({
         where: {
@@ -48,37 +65,49 @@ export default async function Profile(){
             orderId: userId?.id || ''
         }
     })
-    
 
-    if(session){
-        return (
-            <>
-                <ProfileBox label='Meus Pedidos'>
-                    {
-                        orders.length > 0 && (
-                            orders.map(order => <Order 
-                                                    key={order.id} 
-                                                    createdAt={order.createdAt} 
-                                                    pizzaImage={order.firstPizzaImg} 
-                                                    totalPrice={order.totalPrice} 
-                                                    paymentType={payment[order.paymentMethod as 'money' | 'credit' | 'pix'] }
-                                                    pizzas={JSON.parse(order.pizzas)}
-                                                    status={pizzaStatus[order.successfull as 'pending' | 'finished'| 'canceled']}
-                                                />)
-                        ) 
-                    }
-                </ProfileBox>
+    const addressIdStreetAndNumber = addressesData.map(address => {
+        return {
+            id: address.id,
+            street: address.street,
+            houseNumber: address.houseNumber
+        }
+    })
     
-                <ProfileBox label='Informações Pessoais'>
-                    <Link href='/profile/edit' className="hover:brightness-125 active:scale-95 bg-primary text-white px-5 py-3 font-medium text-lg md:text-2xl flex gap-3 items-center rounded">
-                       <PencilSimple className="h-5 w-5 md:h-[28px] md:w-[28px]" weight="bold" /> 
-                       Mais Informações
-                    </Link>
-                </ProfileBox>
-            </>
-        )
-    }
-    else {
-        redirect('/login')
-    }
+    return (
+        <>
+            <h1 className="mt-5 mb-6 md:mb-12 text-3xl md:text-4xl xl:5xl text-primary font-bold">Meu Perfil</h1>
+
+        
+            <div className="flex flex-col lg:flex-row gap-x-12 w-full max-w-full px-3 sm:px-5 md:px-6">            
+                <FormProfile profileData={profileData} addressIdStreetAndNumber={addressIdStreetAndNumber}/>
+
+                <div className="flex flex-col lg:flex-1 lg:max-w-[66%] xl:max-w-[68%] 2xl:max-w-[1400px]">
+                    <ProfileBox label='Meus Pedidos'>
+                        <OrdersBox orders={orders} />
+                    </ProfileBox>
+        
+                    <ProfileBox label='Endereços'>
+                        <div className="w-full">
+                            <AddressesBox addresses={addressesData} />
+
+                            {/* Link para adicionar novo endereço */}
+                            <div className="flex gap-2">
+                                <Link 
+                                    href='/profile/add-address' 
+                                    className={
+                                        "bg-primary text-white px-5 py-3 font-medium text-base md:text-lg flex gap-3 items-center rounded max-w-fit " +
+                                        "hover:brightness-125 active:scale-95"
+                                    }
+                                >
+                                    <Plus className="h-4 w-4 md:h-5 md:w-5" weight="bold" /> 
+                                    Adicionar endereço
+                                </Link>
+                            </div>
+                        </div>
+                    </ProfileBox>
+                </div>
+            </div>
+        </>
+    )
 }
