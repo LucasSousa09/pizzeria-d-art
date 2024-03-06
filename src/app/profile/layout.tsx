5eba40ae6f0d0b382d20079617644bc7bbeb382b
lucas <lucasrodrigues.sousa09@gmail.com>
2024-03-06 16:50:00 -0300

    Update profile styles layout

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -4,8 +4,7 @@ export default function ProfileLayout({
     children: React.ReactNode
   }) {
     return (
-      <div className="px-5 2xl:px-0 flex flex-col items-center justify-start min-h-[calc(100vh-104px)] mt-[60px] md:mt-[104px]">
-        <h1 className="mt-5 text-5xl text-primary font-bold">Meu Perfil</h1>
+      <div className="flex flex-col items-center justify-start max-w-[100vw] min-h-[calc(100vh-60px)] md:min-h-[calc(100vh-104px)] mt-[60px] md:mt-[104px]">
         {children}
       </div>    
     )
