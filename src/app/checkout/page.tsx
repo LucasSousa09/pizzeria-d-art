4e99ed4b8fe1b921ac9d99d9537dc59a307587da
lucas <lucasrodrigues.sousa09@gmail.com>
2024-03-06 16:47:44 -0300

    Update checkout page fetching add errors handling

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,27 +1,42 @@
 import { redirect } from "next/navigation";
 import { getServerSession } from 'next-auth';
 
-import { getProfileData } from '@/utils/getProfileData';
-
-import type { ProfileDataProps } from '../profile/edit/page';
+import type { AddressData } from '../../components/Form/FormAddress';
 
 import { FormCheckout } from '@/components/Form/FormCheckout';
+
+import { api } from "@/lib/axios";
+
+type ProfileData = {
+    phone: string | null
+}
 
 export default async function CheckoutPage(){
     const session = await getServerSession()
 
-    if(session){
-        const data:ProfileDataProps = await getProfileData(session?.user?.email || '')
+    if(session && session.user){
+        const { data: addresses } : { data: AddressData[] } = await api.post('/get-addresses-data', {email: session.user.email})
+        const { data: profile } : { data: ProfileData } = await api.post('/get-profile-data', {email: session.user.email})
+
+        if(addresses.length === 0){
+            redirect('/profile/add-address?error=missing+address')
+        }
+
+        if(profile.phone === null){
+            redirect('/profile?error=missing+phone')
+        }
+
+        const mainAddress = addresses.filter(address => address.mainAddress === true)
 
         const checkoutData = {
-            street: data?.street,
-            district: data?.district,
-            zipCode: data?.zipCode,
-            state: data?.state,
-            houseNumber: data?.houseNumber,
-            complement: data?.complement,
-            city: data?.city,
-            reference: data?.reference
+            street: mainAddress[0].street,
+            district: mainAddress[0].district,
+            zipCode: mainAddress[0].zipCode,
+            state: mainAddress[0].state,
+            houseNumber: mainAddress[0].houseNumber,
+            complement: mainAddress[0].complement,
+            city: mainAddress[0].city,
+            reference: mainAddress[0].reference
         }
         
         return (
